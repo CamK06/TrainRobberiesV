bff15c9816ebbdf58c17632602740c214b495d82
Starman <CamdenKerby@gmail.com>
2020-05-11 17:12:10 -0400

    Added project files

---
+++ TrainRobberiesV/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using GTA;
+using GTA.Math;
+using GTA.Native;
+
+namespace TrainRobberiesV
+{
+    public class Main : Script
+    {
+        public Main()
+        {
+            UI.Notify("Train Robberies V started successfully!");
+            Tick += OnTick;
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
